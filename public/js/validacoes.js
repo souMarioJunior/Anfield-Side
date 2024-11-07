50f8f74c254cc38fc1fb7a4e01d0f1aa0d940687
souMarioJunior <mario.aaraujojr@sptech.school>
2024-11-07 13:19:54 -0300

    Login com banco de dados

--- public/js/validacoes.js
+++ public/js/validacoes.js
@@ -220,3 +220,68 @@ function validarCadastro() {
 
   console.log(selectJogadores);
 }
+
+// VALIDAÇÕES DE LOGIN
+// -----------------------------------------------------------------------------------------------------
+
+function validarLogin() {
+  // aguardar();
+
+  var emailVar = document.getElementById("input_email").value;
+  var senhaVar = document.getElementById("input_senha").value;
+
+  if (emailVar == "" || senhaVar == "") {
+      cardErro.style.display = "block"
+      mensagem_erro.innerHTML = "(Mensagem de erro para todos os campos em branco)";
+      finalizarAguardar();
+      return false;
+  }
+  
+  console.log("FORM LOGIN: ", emailVar);
+  console.log("FORM SENHA: ", senhaVar);
+
+  fetch("/usuarios/autenticar", {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+          emailServer: emailVar,
+          senhaServer: senhaVar
+      })
+  }).then(function (resposta) {
+      console.log("ESTOU NO THEN DO entrar()!")
+
+      if (resposta.ok) {
+          console.log(resposta);
+
+          resposta.json().then(json => {
+              console.log(json);
+              console.log(JSON.stringify(json));
+              sessionStorage.EMAIL_USUARIO = json.email;
+              sessionStorage.NOME_USUARIO = json.nome;
+              sessionStorage.ID_USUARIO = json.id;
+              sessionStorage.AQUARIOS = JSON.stringify(json.aquarios)
+
+              setTimeout(function () {
+                  window.location = "./feed.html";
+              }, 1000); // apenas para exibir o loading
+
+          });
+
+      } else {
+
+          console.log("Houve um erro ao tentar realizar o login!");
+
+          resposta.text().then(texto => {
+              console.error(texto);
+              finalizarAguardar(texto);
+          });
+      }
+
+  }).catch(function (erro) {
+      console.log(erro);
+  })
+
+  return false;
+}
--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -19,21 +19,14 @@ function autenticar(req, res) {
 
                     if (resultadoAutenticar.length == 1) {
                         console.log(resultadoAutenticar);
+                        res.json({
+                            idUsuario: resultadoAutenticar[0].id,
+                            nomeCompleto: resultadoAutenticar[0].nome,
+                            email: resultadoAutenticar[0].email,
+                            senha: resultadoAutenticar[0].senha
+                            // aquarios: resultadoAquarios
+                        });
 
-                        aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
-                            .then((resultadoAquarios) => {
-                                if (resultadoAquarios.length > 0) {
-                                    res.json({
-                                        id: resultadoAutenticar[0].id,
-                                        email: resultadoAutenticar[0].email,
-                                        nome: resultadoAutenticar[0].nome,
-                                        senha: resultadoAutenticar[0].senha,
-                                        aquarios: resultadoAquarios
-                                    });
-                                } else {
-                                    res.status(204).json({ aquarios: [] });
-                                }
-                            })
                     } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
                     } else {
