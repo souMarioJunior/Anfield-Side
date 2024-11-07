64bc5fa8a465214c4396c8506c08a22a951df522
souMarioJunior <mario.aaraujojr@sptech.school>
2024-11-06 21:57:21 -0300

    Fix validações cadastro

--- js/validacoes.js
+++ js/validacoes.js
@@ -1,34 +1,22 @@
-var guardar_nome = "";
-var guardar_email = "";
-var guardar_senha = "";
-var guardar_senha = "";
-var guardar_repetir_senha = "";
-
-function validarCadastro() {
-//   var nomeCompleto = document.getElementById("ipt_cad_nome").value;
-//   var email = document.getElementById("ipt_cad_email").value;
-//   var senha = document.getElementById("ipt_cad_senha").value;
-//   var repetirSenha = document.getElementById("ipt_cad_repetir_senha").value;
-//   console.log(nomeCompleto, email, senha, repetirSenha);
-
-var modal = document.querySelector(".modal");
-
-modal.style.display = "flex";
-}
+var validacaoNome = false;
+var validacaoEmail = false;
+var validacaoEmail = false;
+var validacaoSenha = false;
+var validacaoRepetirSenha = false;
+var validacaoSelect = false;
 
 function mostrarErrosCadastro() {
   var nomeCompleto = document.getElementById("ipt_cad_nome").value;
-  var email = document.getElementById("ipt_cad_email").value;
-  var senha = document.getElementById("ipt_cad_senha").value;
-  var repetirSenha = document.getElementById("ipt_cad_repetir_senha").value;
 
   // Validação nome
   if (nomeCompleto.length < 3) {
+    validacaoNome = false;
     document.getElementById("ipt_cad_nome").style.color = "#dc2626";
     document.getElementById("campo_nome").style.border = "solid #dc2626 thin";
     document.getElementById("campo_nome").style.borderRadius = "4px";
     document.getElementById("erro_nome").style.display = "block";
   } else {
+    validacaoNome = true;
     document.getElementById("campo_nome").style.border = "none";
     document.getElementById("ipt_cad_nome").style.color = "#d9d9d9";
     document.getElementById("campo_nome").style.borderRadius = "none";
@@ -45,11 +33,13 @@ function mostrarErrosCadastroEmail() {
     email.length < 7 ||
     !email.includes("@")
   ) {
+    validacaoEmail = false;
     document.getElementById("ipt_cad_email").style.color = "#dc2626";
     document.getElementById("campo_email").style.border = "solid #dc2626 thin";
     document.getElementById("campo_email").style.borderRadius = "4px";
     document.getElementById("erro_email").style.display = "block";
   } else {
+    validacaoEmail = true;
     document.getElementById("campo_email").style.border = "none";
     document.getElementById("ipt_cad_email").style.color = "#d9d9d9";
     document.getElementById("campo_email").style.borderRadius = "none";
@@ -62,13 +52,15 @@ function mostrarErrosCadastroSenha() {
 
   // Validação de senha
   if (senha.length < 8) {
+    validacaoSenha = false
     document.getElementById("ipt_cad_senha").style.color = "#dc2626";
     document.getElementById("campo_senha").style.border = "solid #dc2626 thin";
     document.getElementById("campo_senha").style.borderRadius = "4px";
     document.getElementById("erro_senha").style.display = "block";
-
+    
     console.log(senha < 8);
   } else {
+    validacaoSenha = true;
     document.getElementById("campo_senha").style.border = "none";
     document.getElementById("ipt_cad_senha").style.color = "#d9d9d9";
     document.getElementById("campo_senha").style.borderRadius = "none";
@@ -80,19 +72,119 @@ function mostrarErrosConfirmarSenha() {
   var senha = document.getElementById("ipt_cad_senha").value;
   var repetirSenha = document.getElementById("ipt_cad_repetir_senha").value;
 
-    // Validação de senhas iguais
-    if(repetirSenha != senha) {
+  // Validação de senhas iguais
+  if (repetirSenha != senha) {
+    validacaoRepetirSenha = false;
     document.getElementById("ipt_cad_repetir_senha").style.color = "#dc2626";
-    document.getElementById("campo_repetir_senha").style.border = "solid #dc2626 thin";
+    document.getElementById("campo_repetir_senha").style.border =
+      "solid #dc2626 thin";
     document.getElementById("campo_repetir_senha").style.borderRadius = "4px";
     document.getElementById("erro_repetir_senha").style.display = "block";
 
     console.log(senha < 8);
   } else {
+    validacaoRepetirSenha = true;
     document.getElementById("campo_repetir_senha").style.border = "none";
     document.getElementById("ipt_cad_repetir_senha").style.color = "#d9d9d9";
     document.getElementById("campo_repetir_senha").style.borderRadius = "none";
     document.getElementById("erro_repetir_senha").style.display = "none";
   }
+}
+
+function validarCadastro() {
+  var nomeCompleto = document.getElementById("ipt_cad_nome").value;
+  var email = document.getElementById("ipt_cad_email").value;
+  var senha = document.getElementById("ipt_cad_senha").value;
+  var repetirSenha = document.getElementById("ipt_cad_repetir_senha").value;
+  var selectJogadores = document.getElementById("slc_jogador").value;
+  var modal = document.querySelector(".modal");
+
+    // Validação nome
+    if (nomeCompleto.length < 3) {
+      validacaoNome = false;
+      document.getElementById("ipt_cad_nome").style.color = "#dc2626";
+      document.getElementById("campo_nome").style.border = "solid #dc2626 thin";
+      document.getElementById("campo_nome").style.borderRadius = "4px";
+      document.getElementById("erro_nome").style.display = "block";
+    } else {
+      validacaoNome = true;
+      document.getElementById("campo_nome").style.border = "none";
+      document.getElementById("ipt_cad_nome").style.color = "#d9d9d9";
+      document.getElementById("campo_nome").style.borderRadius = "none";
+      document.getElementById("erro_nome").style.display = "none";
+    }
+
+      // Validação email
+  if (
+    (!email.includes(".com") && !email.includes(".com.br")) ||
+    email.length < 7 ||
+    !email.includes("@")
+  ) {
+    validacaoEmail = false;
+    document.getElementById("ipt_cad_email").style.color = "#dc2626";
+    document.getElementById("campo_email").style.border = "solid #dc2626 thin";
+    document.getElementById("campo_email").style.borderRadius = "4px";
+    document.getElementById("erro_email").style.display = "block";
+  } else {
+    validacaoEmail = true;
+    document.getElementById("campo_email").style.border = "none";
+    document.getElementById("ipt_cad_email").style.color = "#d9d9d9";
+    document.getElementById("campo_email").style.borderRadius = "none";
+    document.getElementById("erro_email").style.display = "none";
+  }
+  
+  // Validação de senha
+  if (senha.length < 8) {
+    validacaoSenha = false
+    document.getElementById("ipt_cad_senha").style.color = "#dc2626";
+    document.getElementById("campo_senha").style.border = "solid #dc2626 thin";
+    document.getElementById("campo_senha").style.borderRadius = "4px";
+    document.getElementById("erro_senha").style.display = "block";
     
+    console.log(senha < 8);
+  } else {
+    validacaoSenha = true;
+    document.getElementById("campo_senha").style.border = "none";
+    document.getElementById("ipt_cad_senha").style.color = "#d9d9d9";
+    document.getElementById("campo_senha").style.borderRadius = "none";
+    document.getElementById("erro_senha").style.display = "none";
+  }
+
+    // Validação de senhas iguais
+    if (repetirSenha != senha || repetirSenha == "") {
+      validacaoRepetirSenha = false;
+      document.getElementById("ipt_cad_repetir_senha").style.color = "#dc2626";
+      document.getElementById("ipt_cad_repetir_senha").style.color = "#dc2626";
+      document.getElementById("campo_repetir_senha").style.border =
+        "solid #dc2626 thin";
+      document.getElementById("campo_repetir_senha").style.borderRadius = "4px";
+      document.getElementById("erro_repetir_senha").style.display = "block";
+  
+      console.log(senha < 8);
+    } else {
+      validacaoRepetirSenha = true;
+      document.getElementById("campo_repetir_senha").style.border = "none";
+      document.getElementById("ipt_cad_repetir_senha").style.color = "#d9d9d9";
+      document.getElementById("campo_repetir_senha").style.borderRadius = "none";
+      document.getElementById("erro_repetir_senha").style.display = "none";
+    }
+
+    // Validação select
+    if (selectJogadores == "") {
+    validacaoSelect = false;
+    document.getElementById("campo_select").style.border = "solid #dc2626 thin";
+    document.getElementById("campo_select").style.borderRadius = "4px";
+    document.getElementById("erro_selecionar_jogador").style.display = "block";
+  } else {
+    validacaoSelect = true;
+    document.getElementById("campo_select").style.border = "none";
+    document.getElementById("ipt_cad_nome").style.color = "#d9d9d9";
+    document.getElementById("campo_nome").style.borderRadius = "none";
+    document.getElementById("erro_selecionar_jogador").style.display = "none";
+  }
+  if (validacaoNome == true && validacaoEmail == true && validacaoSenha == true && validacaoRepetirSenha == true && validacaoSelect == true) {
+    modal.style.display = "flex";
+  }
+
+  console.log(selectJogadores);
 }
