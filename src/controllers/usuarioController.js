54f6f7d38cf390981b6d2075c0f32775b8365fd4
souMarioJunior <mario.aaraujojr@sptech.school>
2024-11-07 09:38:31 -0300

    Add integração cadastro

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -53,24 +53,31 @@ function autenticar(req, res) {
 
 function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
-    var nome = req.body.nomeServer;
+    var nomeCompleto = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
-    var fkEmpresa = req.body.idEmpresaVincularServer;
+    var jogadorFavorito = req.body.jogadorServer
+    // var fkEmpresa = req.body.idEmpresaVincularServer;
 
     // Faça as validações dos valores
-    if (nome == undefined) {
+    if (nomeCompleto == undefined) {
         res.status(400).send("Seu nome está undefined!");
     } else if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
-    } else if (fkEmpresa == undefined) {
+    } else if (jogadorFavorito == undefined) {
         res.status(400).send("Sua empresa a vincular está undefined!");
     } else {
 
+//         idUsuario int primary key auto_increment,
+// nomeCompleto varchar(100),
+// email varchar(319),
+// senha varchar(28),
+// jogadorFavorito varchar(30),
+
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, fkEmpresa)
+        usuarioModel.cadastrar(nomeCompleto, email, senha, jogadorFavorito)
             .then(
                 function (resultado) {
                     res.json(resultado);
--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -1,62 +1,39 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+create database anfieldSide;
 
-/*
-comandos para mysql server
-*/
+use anfieldSide;
 
-CREATE DATABASE aquatech;
-
-USE aquatech;
-
-CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14),
-	codigo_ativacao VARCHAR(50)
+create table usuario (
+idUsuario int primary key auto_increment,
+nomeCompleto varchar(100),
+email varchar(319),
+senha varchar(28),
+jogadorFavorito varchar(30),
+usuarioLikes int,
+usuarioDeslikes int,
+bio varchar(120)
 );
 
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+create table quiz (
+idQuiz int primary key auto_increment,
+acertos int,
+erros int,
+tentativas int,
+fkUsuario int,
+constraint foreignKeyUsuario foreign key (fkUsuario) references usuario (idUsuario)
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+create table manchete (
+idManchete int primary key auto_increment,
+titulo varchar(15),
+descricao varchar(500),
+noticiaLikes int,
+noticiaDeslikes int
 );
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
-);
-
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
-
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
-insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);
+create table interacao (
+fkUsuario int,
+fkManchete int,
+constraint foreignKeyUsuarioInteracao foreign key (fkUsuario) references usuario (idUsuario),
+constraint foreignKeyMancheteInteracao foreign key (fkManchete) references manchete (idManchete),
+constraint pks primary key(fkUsuario, fkManchete)
+)
