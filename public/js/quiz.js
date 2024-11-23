a3825931b9e0c521be0d23a250ab804565fc6ace
souMarioJunior <mario.aaraujojr@sptech.school>
2024-11-23 15:22:51 -0300

    Add quiz (50 perguntas até agora)

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -1,72 +1,576 @@
-const btns = document.querySelector(".btns_game");
+var todas_as_perguntas = [
+  (pergunta_00 = {
+    numeroPergunta: 0,
+    pergunta: "Qual é o apelido do Liverpool FC?",
+    btn_a: "The Blues",
+    btn_b: "The Reds",
+    btn_c: "The Gunners",
+    btn_d: "The Citizens",
+    correta: "b",
+  }),
+  (pergunta_01 = {
+    numeroPergunta: 1,
+    pergunta: "Qual é o estádio do Liverpool FC?",
+    btn_a: "Old Trafford",
+    btn_b: "Anfield",
+    btn_c: "Stamford Bridge",
+    btn_d: "Etihad Stadium",
+    correta: "b",
+  }),
+  (pergunta_02 = {
+    numeroPergunta: 2,
+    pergunta: "Quantas vezes o Liverpool venceu a Liga dos Campeões da UEFA?",
+    btn_a: "3",
+    btn_b: "6",
+    btn_c: "5",
+    btn_d: "7",
+    correta: "b",
+  }),
+  (pergunta_03 = {
+    numeroPergunta: 3,
+    pergunta: "Quem é o maior artilheiro da história do Liverpool FC?",
+    btn_a: "Kenny Dalglish",
+    btn_b: "Steven Gerrard",
+    btn_c: "Ian Rush",
+    btn_d: "Robbie Fowler",
+    correta: "c",
+  }),
+  (pergunta_04 = {
+    numeroPergunta: 4,
+    pergunta: "Quem é o atual treinador do Liverpool FC?",
+    btn_a: "Pep Guardiola",
+    btn_b: "Jurgen Klopp",
+    btn_c: "Jose Mourinho",
+    btn_d: "Carlo Ancelotti",
+    correta: "b",
+  }),
+  (pergunta_05 = {
+    numeroPergunta: 5,
+    pergunta: "Em que ano o Liverpool foi fundado?",
+    btn_a: "1888",
+    btn_b: "1892",
+    btn_c: "1902",
+    btn_d: "1910",
+    correta: "b",
+  }),
+  (pergunta_06 = {
+    numeroPergunta: 6,
+    pergunta:
+      "Qual jogador marcou o gol da vitória na final da Liga dos Campeões de 2019?",
+    btn_a: "Mohamed Salah",
+    btn_b: "Roberto Firmino",
+    btn_c: "Sadio Mané",
+    btn_d: "Divock Origi",
+    correta: "d",
+  }),
+  (pergunta_07 = {
+    numeroPergunta: 7,
+    pergunta:
+      "Quantos títulos da Premier League o Liverpool conquistou até 2021?",
+    btn_a: "19",
+    btn_b: "18",
+    btn_c: "20",
+    btn_d: "17",
+    correta: "a",
+  }),
+  (pergunta_08 = {
+    numeroPergunta: 8,
+    pergunta: "Qual é o maior rival do Liverpool?",
+    btn_a: "Manchester City",
+    btn_b: "Manchester United",
+    btn_c: "Chelsea",
+    btn_d: "Everton",
+    correta: "b",
+  }),
+  (pergunta_09 = {
+    numeroPergunta: 9,
+    pergunta:
+      "Quem foi o capitão do Liverpool na final da Liga dos Campeões de 2005?",
+    btn_a: "Jamie Carragher",
+    btn_b: "Steven Gerrard",
+    btn_c: "Xabi Alonso",
+    btn_d: "Sami Hyypiä",
+    correta: "b",
+  }),
+  (pergunta_10 = {
+    numeroPergunta: 10,
+    pergunta:
+      "Em que ano o Liverpool venceu seu primeiro título da Premier League?",
+    btn_a: "1990",
+    btn_b: "2020",
+    btn_c: "2005",
+    btn_d: "1984",
+    correta: "b",
+  }),
+  (pergunta_11 = {
+    numeroPergunta: 11,
+    pergunta:
+      "Qual foi o resultado da final da Liga dos Campeões de 2005 entre Liverpool e Milan?",
+    btn_a: "Liverpool 1-0 Milan",
+    btn_b: "Liverpool 3-3 Milan (Liverpool venceu nos pênaltis)",
+    btn_c: "Liverpool 2-1 Milan",
+    btn_d: "Liverpool 4-3 Milan",
+    correta: "b",
+  }),
+  (pergunta_12 = {
+    numeroPergunta: 12,
+    pergunta:
+      "Qual jogador marcou o gol da vitória na final da Liga dos Campeões de 2005?",
+    btn_a: "Steven Gerrard",
+    btn_b: "Vladimir Smicer",
+    btn_c: "Xabi Alonso",
+    btn_d: "Jerzy Dudek",
+    correta: "c",
+  }),
+  (pergunta_13 = {
+    numeroPergunta: 13,
+    pergunta: "Quantos títulos da FA Cup o Liverpool venceu até 2021?",
+    btn_a: "7",
+    btn_b: "8",
+    btn_c: "10",
+    btn_d: "9",
+    correta: "b",
+  }),
+  (pergunta_14 = {
+    numeroPergunta: 14,
+    pergunta: "Qual é o lema do Liverpool FC?",
+    btn_a: "You'll Never Walk Alone",
+    btn_b: "To Dare is to Do",
+    btn_c: "Glory, Glory Man United",
+    btn_d: "Come on You Blues",
+    correta: "a",
+  }),
+  (pergunta_15 = {
+    numeroPergunta: 15,
+    pergunta: "Qual jogador do Liverpool venceu a Bola de Ouro em 2001?",
+    btn_a: "Steven Gerrard",
+    btn_b: "Michael Owen",
+    btn_c: "Luis Suárez",
+    btn_d: "Ian Rush",
+    correta: "b",
+  }),
+  (pergunta_16 = {
+    numeroPergunta: 16,
+    pergunta:
+      "Qual era o treinador do Liverpool durante a temporada invicta de 1987-1988 na liga?",
+    btn_a: "Bob Paisley",
+    btn_b: "Kenny Dalglish",
+    btn_c: "Joe Fagan",
+    btn_d: "Graeme Souness",
+    correta: "b",
+  }),
+  (pergunta_17 = {
+    numeroPergunta: 17,
+    pergunta:
+      "Quantos gols Roger Hunt marcou pelo Liverpool em todas as competições?",
+    btn_a: "244",
+    btn_b: "228",
+    btn_c: "256",
+    btn_d: "275",
+    correta: "a",
+  }),
+  (pergunta_18 = {
+    numeroPergunta: 18,
+    pergunta:
+      "Qual jogador do Liverpool é conhecido como 'God' pelos torcedores?",
+    btn_a: "Steven Gerrard",
+    btn_b: "Robbie Fowler",
+    btn_c: "Kenny Dalglish",
+    btn_d: "Luis Suárez",
+    correta: "b",
+  }),
+  (pergunta_19 = {
+    numeroPergunta: 19,
+    pergunta:
+      "Quem era o goleiro do Liverpool na final da Liga dos Campeões de 2018?",
+    btn_a: "Alisson Becker",
+    btn_b: "Loris Karius",
+    btn_c: "Pepe Reina",
+    btn_d: "Simon Mignolet",
+    correta: "b",
+  }),
+  (pergunta_20 = {
+    numeroPergunta: 20,
+    pergunta:
+      "Qual jogador marcou um hat-trick contra o Arsenal em dezembro de 2018?",
+    btn_a: "Mohamed Salah",
+    btn_b: "Roberto Firmino",
+    btn_c: "Sadio Mané",
+    btn_d: "Divock Origi",
+    correta: "b",
+  }),
+  (pergunta_21 = {
+    numeroPergunta: 21,
+    pergunta:
+      "Quantos títulos de liga inglesa o Liverpool conquistou até 2021?",
+    btn_a: "18",
+    btn_b: "19",
+    btn_c: "20",
+    btn_d: "21",
+    correta: "b",
+  }),
+  (pergunta_22 = {
+    numeroPergunta: 22,
+    pergunta:
+      "Quem marcou o gol da vitória na final da Liga dos Campeões de 2019?",
+    btn_a: "Roberto Firmino",
+    btn_b: "Mohamed Salah",
+    btn_c: "Divock Origi",
+    btn_d: "Sadio Mané",
+    correta: "c",
+  }),
+  (pergunta_23 = {
+    numeroPergunta: 23,
+    pergunta: "Qual é a capacidade atual do estádio Anfield?",
+    btn_a: "45.000",
+    btn_b: "50.000",
+    btn_c: "54.000",
+    btn_d: "60.000",
+    correta: "c",
+  }),
+  (pergunta_24 = {
+    numeroPergunta: 24,
+    pergunta: "Quem foi o técnico do Liverpool antes de Jurgen Klopp?",
+    btn_a: "Kenny Dalglish",
+    btn_b: "Brendan Rodgers",
+    btn_c: "Roy Hodgson",
+    btn_d: "Rafa Benítez",
+    correta: "b",
+  }),
+  (pergunta_25 = {
+    numeroPergunta: 25,
+    pergunta:
+      "Qual jogador do Liverpool é conhecido por sua famosa comemoração de tocar a bandeira de escanteio?",
+    btn_a: "Steven Gerrard",
+    btn_b: "Mohamed Salah",
+    btn_c: "Roberto Firmino",
+    btn_d: "Luis Suárez",
+    correta: "b",
+  }),
+  (pergunta_26 = {
+    numeroPergunta: 26,
+    pergunta:
+      "Em que ano o Liverpool mudou para seu uniforme vermelho tradicional?",
+    btn_a: "1896",
+    btn_b: "1901",
+    btn_c: "1964",
+    btn_d: "1952",
+    correta: "c",
+  }),
+  (pergunta_27 = {
+    numeroPergunta: 27,
+    pergunta:
+      "Qual jogador marcou o gol da vitória na final da Liga dos Campeões de 2005?",
+    btn_a: "Steven Gerrard",
+    btn_b: "Vladimir Smicer",
+    btn_c: "Xabi Alonso",
+    btn_d: "Jerzy Dudek",
+    correta: "c",
+  }),
+  (pergunta_28 = {
+    numeroPergunta: 28,
+    pergunta:
+      "Quantos gols Roger Hunt marcou pelo Liverpool em todas as competições?",
+    btn_a: "244",
+    btn_b: "228",
+    btn_c: "256",
+    btn_d: "275",
+    correta: "a",
+  }),
+  (pergunta_29 = {
+    numeroPergunta: 29,
+    pergunta: "Qual é o maior rival do Liverpool?",
+    btn_a: "Manchester City",
+    btn_b: "Manchester United",
+    btn_c: "Chelsea",
+    btn_d: "Everton",
+    correta: "b",
+  }),
+  (pergunta_30 = {
+    numeroPergunta: 30,
+    pergunta: "Quem foi o técnico do Liverpool antes de Jurgen Klopp?",
+    btn_a: "Kenny Dalglish",
+    btn_b: "Brendan Rodgers",
+    btn_c: "Roy Hodgson",
+    btn_d: "Rafa Benítez",
+    correta: "b",
+  }),
+  (pergunta_31 = {
+    numeroPergunta: 31,
+    pergunta:
+      "Quem marcou o primeiro gol do Liverpool na final da Liga dos Campeões de 2019?",
+    btn_a: "Mohamed Salah",
+    btn_b: "Sadio Mané",
+    btn_c: "Virgil van Dijk",
+    btn_d: "Divock Origi",
+    correta: "a",
+  }),
+  (pergunta_32 = {
+    numeroPergunta: 32,
+    pergunta:
+      "Em que ano o Liverpool conquistou seu primeiro título da Premier League?",
+    btn_a: "2020",
+    btn_b: "1990",
+    btn_c: "2005",
+    btn_d: "1984",
+    correta: "a",
+  }),
+  (pergunta_33 = {
+    numeroPergunta: 33,
+    pergunta: "Qual jogador do Liverpool é conhecido como 'King Kenny'?",
+    btn_a: "Steven Gerrard",
+    btn_b: "Kenny Dalglish",
+    btn_c: "Robbie Fowler",
+    btn_d: "Graeme Souness",
+    correta: "b",
+  }),
+  (pergunta_34 = {
+    numeroPergunta: 34,
+    pergunta:
+      "Quem era o capitão do Liverpool na final da Liga dos Campeões de 2005?",
+    btn_a: "Jamie Carragher",
+    btn_b: "Steven Gerrard",
+    btn_c: "Sami Hyypiä",
+    btn_d: "Dietmar Hamann",
+    correta: "b",
+  }),
+  (pergunta_35 = {
+    numeroPergunta: 35,
+    pergunta:
+      "Quantos gols Ian Rush marcou pelo Liverpool em todas as competições?",
+    btn_a: "244",
+    btn_b: "228",
+    btn_c: "256",
+    btn_d: "346",
+    correta: "d",
+  }),
+  
+  (pergunta_36 = {
+    numeroPergunta: 36,
+    pergunta:
+      "Quem marcou o gol da vitória na final da Liga dos Campeões de 2005?",
+    btn_a: "Steven Gerrard",
+    btn_b: "Vladimir Smicer",
+    btn_c: "Xabi Alonso",
+    btn_d: "Jerzy Dudek",
+    correta: "c",
+  }),
 
-function jogar() {
-  var infos = document.querySelector(".box_infos");
-  var jogo = document.querySelector(".box_jogo");
+  (pergunta_37 = {
+    numeroPergunta: 37,
+    pergunta:
+      "Em que ano o Liverpool mudou para seu uniforme vermelho tradicional?",
+    btn_a: "1896",
+    btn_b: "1901",
+    btn_c: "1964",
+    btn_d: "1952",
+    correta: "c",
+  }),
 
-  infos.style.display = "none";
-  jogo.style.display = "flex";
-  proximaPergunta();
-}
+  (pergunta_38 = {
+    numeroPergunta: 38,
+    pergunta:
+      "Qual jogador do Liverpool é conhecido por sua famosa comemoração de tocar a bandeira de escanteio?",
+    btn_a: "Steven Gerrard",
+    btn_b: "Mohamed Salah",
+    btn_c: "Roberto Firmino",
+    btn_d: "Luis Suárez",
+    correta: "b",
+  }),
 
-var perguntas = [
-  {
-    "pergunta": "Qual é o estádio do Liverpool FC?",
-    "respostas": [
-        { "resposta": "Old Trafford", "correcao": false },
-        { "resposta": "Anfield", "correcao": true },
-        { "resposta": "Stamford Bridge", "correcao": false },
-        { "resposta": "Etihad Stadium", "correcao": false }
-    ]
-},
+  (pergunta_39 = {
+    numeroPergunta: 39,
+    pergunta:
+      "Quantos gols Ian Rush marcou pelo Liverpool em todas as competições?",
+    btn_a: "244",
+    btn_b: "228",
+    btn_c: "256",
+    btn_d: "346",
+    correta: "d",
+  }),
 
-{
-  "pergunta": "Quantas vezes o Liverpool venceu a Liga dos Campeões da UEFA?",
-  "respostas": [
-      { "resposta": "3", "correcao": false },
-      { "resposta": "6", "correcao": true },
-      { "resposta": "5", "correcao": false },
-      { "resposta": "7", "correcao": false }
-  ]
-},
+  (pergunta_40 = {
+    numeroPergunta: 40,
+    pergunta: "Qual é a capacidade atual do estádio Anfield?",
+    btn_a: "45.000",
+    btn_b: "50.000",
+    btn_c: "54.000",
+    btn_d: "60.000",
+    correta: "c",
+  }),
 
-{
-  "pergunta": "Quem é o maior artilheiro da história do Liverpool FC?",
-  "respostas": [
-      { "resposta": "Kenny Dalglish", "correcao": false },
-      { "resposta": "Steven Gerrard", "correcao": false },
-      { "resposta": "Ian Rush", "correcao": true },
-      { "resposta": "Robbie Fowler", "correcao": false }
-  ]
-},
+  (pergunta_41 = {
+    numeroPergunta: 41,
+    pergunta: "Qual jogador do Liverpool é conhecido como 'King Kenny'?",
+    btn_a: "Steven Gerrard",
+    btn_b: "Kenny Dalglish",
+    btn_c: "Robbie Fowler",
+    btn_d: "Graeme Souness",
+    correta: "b",
+  }),
 
-{
-  "pergunta": "Quem é o atual treinador do Liverpool FC?",
-  "respostas": [
-      { "resposta": "Pep Guardiola", "correcao": false },
-      { "resposta": "Jurgen Klopp", "correcao": true },
-      { "resposta": "Jose Mourinho", "correcao": false },
-      { "resposta": "Carlo Ancelotti", "correcao": false }
-  ]
-},
+  (pergunta_42 = {
+    numeroPergunta: 42,
+    pergunta: "Quem foi o técnico do Liverpool antes de Jurgen Klopp?",
+    btn_a: "Kenny Dalglish",
+    btn_b: "Brendan Rodgers",
+    btn_c: "Roy Hodgson",
+    btn_d: "Rafa Benítez",
+    correta: "b",
+  }),
 
-{
-  "pergunta": "Em que ano o Liverpool foi fundado?",
-  "respostas": [
-      { "resposta": "1888", "correcao": false },
-      { "resposta": "1892", "correcao": true },
-      { "resposta": "1902", "correcao": false },
-      { "resposta": "1910", "correcao": false }
-  ]
-}
+  (pergunta_43 = {
+    numeroPergunta: 43,
+    pergunta:
+      "Qual jogador marcou o primeiro gol do Liverpool na final da Liga dos Campeões de 2019?",
+    btn_a: "Mohamed Salah",
+    btn_b: "Sadio Mané",
+    btn_c: "Virgil van Dijk",
+    btn_d: "Divock Origi",
+    correta: "a",
+  }),
+
+  (pergunta_44 = {
+    numeroPergunta: 44,
+    pergunta:
+      "Em que ano o Liverpool conquistou seu primeiro título da Premier League?",
+    btn_a: "2020",
+    btn_b: "1990",
+    btn_c: "2005",
+    btn_d: "1984",
+    correta: "a",
+  }),
+
+  (pergunta_45 = {
+    numeroPergunta: 45,
+    pergunta:
+      "Quem marcou o gol da vitória na final da Liga dos Campeões de 2005?",
+    btn_a: "Steven Gerrard",
+    btn_b: "Vladimir Smicer",
+    btn_c: "Xabi Alonso",
+    btn_d: "Jerzy Dudek",
+    correta: "c",
+  }),
+
+  (pergunta_46 = {
+    numeroPergunta: 46,
+    pergunta:
+      "Quantos gols Roger Hunt marcou pelo Liverpool em todas as competições?",
+    btn_a: "244",
+    btn_b: "228",
+    btn_c: "256",
+    btn_d: "275",
+    correta: "a",
+  }),
+
+  (pergunta_47 = {
+    numeroPergunta: 47,
+    pergunta: "Qual é o maior rival do Liverpool?",
+    btn_a: "Manchester City",
+    btn_b: "Manchester United",
+    btn_c: "Chelsea",
+    btn_d: "Everton",
+    correta: "b",
+  }),
+
+  (pergunta_48 = {
+    numeroPergunta: 48,
+    pergunta: "Quem foi o técnico do Liverpool antes de Jurgen Klopp?",
+    btn_a: "Kenny Dalglish",
+    btn_b: "Brendan Rodgers",
+    btn_c: "Roy Hodgson",
+    btn_d: "Rafa Benítez",
+    correta: "b",
+  }),
+
+  (pergunta_49 = {
+    numeroPergunta: 49,
+    pergunta:
+      "Quem marcou o primeiro gol do Liverpool na final da Liga dos Campeões de 2019?",
+    btn_a: "Mohamed Salah",
+    btn_b: "Sadio Mané",
+    btn_c: "Virgil van Dijk",
+    btn_d: "Divock Origi",
+    correta: "a",
+  }),
+
+  (pergunta_50 = {
+    numeroPergunta: 50,
+    pergunta:
+      "Em que ano o Liverpool conquistou seu primeiro título da Premier League?",
+    btn_a: "2020",
+    btn_b: "1990",
+    btn_c: "2005",
+    btn_d: "1984",
+    correta: "a",
+  }),
 ];
 
-function proximaPergunta() {
-  for(var contador = 0; contador < btns.childNodes.length; contador++) {
-    if(btns.childNodes[contador].tagName == "BUTTON") {
-      btns.removeChild(btns.childNodes[contador])
+var lista_perguntas_aleatorias = [];
+
+function jogar() {
+  var instrucao = document.getElementById("box_infos");
+  var tela_jogo = document.getElementById("box_jogo");
+
+  instrucao.style.display = "none";
+  tela_jogo.style.display = "flex";
+
+  for (var contador = 0; contador < 1; contador++) {
+    var limite = todas_as_perguntas.length;
+    var sorteio = Math.floor(Math.random() * limite);
+
+    if (!lista_perguntas_aleatorias.includes(sorteio)) {
+      lista_perguntas_aleatorias.push(sorteio);
     }
-    break
-    // btns.removeChild(firstChild)
+
+    if (lista_perguntas_aleatorias.length < todas_as_perguntas.length) {
+      contador--;
+    }
+  }
+
+  renderizarPergunta();
+}
+
+var contador = 0;
+var pontos = 0;
+
+function renderizarPergunta() {
+  var renderizar_pergunta = document.getElementById("h2_pergunta");
+  var btn_a = document.getElementById("a");
+  var btn_b = document.getElementById("b");
+  var btn_c = document.getElementById("c");
+  var btn_d = document.getElementById("d");
+  var pontuacao = document.getElementById("pontuacao");
+
+  renderizar_pergunta.innerHTML =
+    todas_as_perguntas[lista_perguntas_aleatorias[contador]].pergunta;
+  btn_a.innerHTML =
+    todas_as_perguntas[lista_perguntas_aleatorias[contador]].btn_a;
+  btn_b.innerHTML =
+    todas_as_perguntas[lista_perguntas_aleatorias[contador]].btn_b;
+  btn_c.innerHTML =
+    todas_as_perguntas[lista_perguntas_aleatorias[contador]].btn_c;
+  btn_d.innerHTML =
+    todas_as_perguntas[lista_perguntas_aleatorias[contador]].btn_d;
+}
+
+function escolher(opcao) {
+  if (contador < todas_as_perguntas.length - 1) {
+    if (
+      opcao == todas_as_perguntas[lista_perguntas_aleatorias[contador]].correta
+    ) {
+      pontos += 10;
+    } else if (pontos > 0) {
+      pontos -= 10;
+    }
+    contador++;
+    pontuacao.innerHTML = `<h1>Sua pontuação atual: ${pontos}</h2>`;
+    renderizarPergunta();
+  } else {
+    console.log("Finalizou");
+    var jogo = document.getElementById("box_jogo");
+    var resultado = document.getElementById("div_resultado");
+    jogo.style.display = "none";
+    resultado.style.display = "flex";
   }
 }
